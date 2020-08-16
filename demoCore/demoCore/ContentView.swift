3670aac96c9ed614376d72ef9a632e68980ac8bd
Fyun <fei.yun.unhi@gmail.com>
2020-08-15 22:57:03 -0400

    add crud

---
+++ demoCore/demoCore/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  demoCore
+//
+//  Created by 云飛 on 8/15/20.
+//  Copyright © 2020 Fei Yun. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+       toDoView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demoCore/demoCore/toDoView.swift
@@ -0,0 +1,125 @@
+//
+//  toDoView.swift
+//  demoCore
+//
+//  Created by 云飛 on 8/15/20.
+//  Copyright © 2020 Fei Yun. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct toDoView: View {
+    @ObservedObject var observedData = observable()
+    @State var todo = ""
+    
+    var body: some View{
+        
+        
+        NavigationView{
+            
+            VStack{
+                    
+                    List{
+                        
+                        ForEach(observedData.data){i in
+                            
+                            Text(i.todo)
+                        }.onDelete { (indexset) in
+                            
+                            self.observedData.deleteTodo(indexset: indexset, id: self.observedData.data[indexset.first!].id)
+                        }
+                }.navigationBarTitle("CoreData")
+                    
+                    HStack{
+                        
+                        TextField("Todos", text: $todo).textFieldStyle(RoundedBorderTextFieldStyle())
+                        Button(action: {
+                            
+                            print(self.todo)
+                            self.observedData.addTodo(todo: self.todo)
+                            self.todo = ""
+                            
+                        }) {
+                            
+                            Text("Add")
+                        }
+                    }.padding()
+            }
+        }
+
+    }
+}
+
+struct toDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        toDoView()
+    }
+}
+
+struct datatype:Identifiable {
+   var id : NSManagedObjectID
+   var todo : String
+}
+
+class observable:ObservableObject{
+    
+    @Published var data=[datatype]()
+    
+    init(){
+        let app=UIApplication.shared.delegate as! AppDelegate
+        let context=app.persistentContainer.viewContext
+        let req=NSFetchRequest<NSFetchRequestResult>(entityName: "Todos")
+        
+        do{
+            let res=try context.fetch(req)
+            
+            for i in res as! [NSManagedObject]{
+                self.data.append(datatype(id: i.objectID, todo: i.value(forKey: "todo") as! String))
+            }
+        }
+        catch{
+            
+        }
+    }
+    //add data
+    func addTodo(todo:String){
+        
+        let app=UIApplication.shared.delegate as! AppDelegate
+        let context=app.persistentContainer.viewContext
+        let entity=NSEntityDescription.insertNewObject(forEntityName: "Todos", into: context)
+        entity.setValue(todo, forKey: "todo")
+        do{
+            try context.save()
+            print("success")
+        }
+        catch{
+            
+        }
+    }
+    //delete data
+    func deleteTodo(indexset:IndexSet,id:NSManagedObjectID){
+        let app=UIApplication.shared.delegate as! AppDelegate
+        let context=app.persistentContainer.viewContext
+        let req=NSFetchRequest<NSFetchRequestResult>(entityName: "Todos")
+        do{
+            let res=try context.fetch(req)
+            
+            for i in res as! [NSManagedObject]{
+                if i.objectID==id{
+                    try context.execute(NSBatchDeleteRequest(objectIDs: [id]))
+                    self.data.remove(atOffsets:indexset)
+                }
+            }
+        }
+        catch{
+            
+        }
+        
+    }
+
+    
+}
+
+
+
